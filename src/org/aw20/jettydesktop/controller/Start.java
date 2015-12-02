0b7b3531fc94e6e941e628d2e818d4d584c87526
Lucy Child <lucy@aw20.co.uk>
2015-12-02 09:27:58 +0000

    old jetty jar removed, build.xml updated and indentation on Start class realigned

--- src/org/aw20/jettydesktop/controller/Start.java
+++ src/org/aw20/jettydesktop/controller/Start.java
@@ -9,9 +9,6 @@ import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
 import java.util.Optional;
 
-import org.aw20.jettydesktop.view.Resources;
-import org.aw20.util.AppFunctions;
-
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.EventHandler;
@@ -26,6 +23,9 @@ import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 import netscape.javascript.JSObject;
+
+import org.aw20.jettydesktop.view.Resources;
+import org.aw20.util.AppFunctions;
 
 
 public class Start extends Application {
@@ -42,99 +42,99 @@ public class Start extends Application {
 
 
 	public static void main( String[] args ) {
-	File f = new File( "resources/" );
-	if ( f.exists() ) {
-		temp = f;
-	}
+		File f = new File( "resources/" );
+		if ( f.exists() ) {
+			temp = f;
+		}
 
-	try ( InputStream htmlBytes = res.getHtmlResource().openStream(); InputStream jsBytes = res.getJSResource().openStream(); InputStream jqueryBytes = res.getJQueryResource().openStream(); InputStream cssBytes = res.getCSSResource().openStream(); InputStream logoBytes = res.getPNGResource().openStream(); ) {
+		try ( InputStream htmlBytes = res.getHtmlResource().openStream(); InputStream jsBytes = res.getJSResource().openStream(); InputStream jqueryBytes = res.getJQueryResource().openStream(); InputStream cssBytes = res.getCSSResource().openStream(); InputStream logoBytes = res.getPNGResource().openStream(); ) {
 
-		Files.copy( htmlBytes, new File( temp, "index.html" ).toPath(), StandardCopyOption.REPLACE_EXISTING );
-		Files.copy( jsBytes, new File( temp, "jetty.js" ).toPath(), StandardCopyOption.REPLACE_EXISTING );
-		Files.copy( jqueryBytes, new File( temp, "jquery-1.11.3.min.js" ).toPath(), StandardCopyOption.REPLACE_EXISTING );
-		Files.copy( cssBytes, new File( temp, "jetty.css" ).toPath(), StandardCopyOption.REPLACE_EXISTING );
-		Files.copy( logoBytes, new File( temp, "logo.png" ).toPath(), StandardCopyOption.REPLACE_EXISTING );
-	} catch ( IOException e ) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
+			Files.copy( htmlBytes, new File( temp, "index.html" ).toPath(), StandardCopyOption.REPLACE_EXISTING );
+			Files.copy( jsBytes, new File( temp, "jetty.js" ).toPath(), StandardCopyOption.REPLACE_EXISTING );
+			Files.copy( jqueryBytes, new File( temp, "jquery-1.11.3.min.js" ).toPath(), StandardCopyOption.REPLACE_EXISTING );
+			Files.copy( cssBytes, new File( temp, "jetty.css" ).toPath(), StandardCopyOption.REPLACE_EXISTING );
+			Files.copy( logoBytes, new File( temp, "logo.png" ).toPath(), StandardCopyOption.REPLACE_EXISTING );
+		} catch ( IOException e ) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
-	launch( args );
+		launch( args );
 	}
 
 
 	@Override
 	public void start( Stage primaryStage ) {
 
-	AnchorPane anchorPane = new AnchorPane();
-	stage = primaryStage;
-	primaryStage.setTitle( "" );
+		AnchorPane anchorPane = new AnchorPane();
+		stage = primaryStage;
+		primaryStage.setTitle( "" );
 
-	final AppFunctions appFunctions = AppFunctions.getInstance();
-	URL index = null;
-	try {
-		index = new File( temp, "index.html" ).toURL();
+		final AppFunctions appFunctions = AppFunctions.getInstance();
+		URL index = null;
+		try {
+			index = new File( temp, "index.html" ).toURL();
 
-	} catch ( MalformedURLException e ) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
+		} catch ( MalformedURLException e ) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
-	JSObject win = (JSObject) webEngine.executeScript( "window" );
-	win.setMember( "app", appFunctions );
-	webView.setContextMenuEnabled( false );
-	webEngine.load( index.toExternalForm() );
-	// Set Layout Constraint
-	AnchorPane.setTopAnchor( webView, 0.0 );
-	AnchorPane.setBottomAnchor( webView, 0.0 );
-	AnchorPane.setLeftAnchor( webView, 0.0 );
-	AnchorPane.setRightAnchor( webView, 0.0 );
+		JSObject win = (JSObject) webEngine.executeScript( "window" );
+		win.setMember( "app", appFunctions );
+		webView.setContextMenuEnabled( false );
+		webEngine.load( index.toExternalForm() );
+		// Set Layout Constraint
+		AnchorPane.setTopAnchor( webView, 0.0 );
+		AnchorPane.setBottomAnchor( webView, 0.0 );
+		AnchorPane.setLeftAnchor( webView, 0.0 );
+		AnchorPane.setRightAnchor( webView, 0.0 );
 
-	// Add WebView to AnchorPane
-	anchorPane.getChildren().add( webView );
+		// Add WebView to AnchorPane
+		anchorPane.getChildren().add( webView );
 
-	primaryScene = new Scene( anchorPane );
+		primaryScene = new Scene( anchorPane );
 
-	primaryStage.setScene( primaryScene );
-	primaryStage.setResizable( true );
-	primaryStage.getIcons().add( new Image( "/org/aw20/jettydesktop/view/logo.png" ) );
-	primaryStage.show();
+		primaryStage.setScene( primaryScene );
+		primaryStage.setResizable( true );
+		primaryStage.getIcons().add( new Image( "/org/aw20/jettydesktop/view/logo.png" ) );
+		primaryStage.show();
 
-	Platform.runLater( new Runnable() {
+		Platform.runLater( new Runnable() {
 
-		public void run() {
+			public void run() {
 
-		Start.stage.setOnCloseRequest( new EventHandler<WindowEvent>() {
+				Start.stage.setOnCloseRequest( new EventHandler<WindowEvent>() {
 
-			public void handle( WindowEvent t ) {
-			if ( appFunctions.serverConfigList == null ) {
-				Platform.exit();
-			} else {
-				int count = 0;
+					public void handle( WindowEvent t ) {
+						if ( appFunctions.serverConfigList == null ) {
+							Platform.exit();
+						} else {
+							int count = 0;
 
-				for ( int i = 0; i < appFunctions.serverConfigList.size(); ++i ) {
-				if ( ( "true" ).equals( appFunctions.serverConfigList.get( i ).getRunning() ) ) {
-					count++;
-				}
-				}
-				if ( count > 0 ) {
-				Alert alert = new Alert( AlertType.CONFIRMATION, "Stop all apps (" + count + ") running ?", ButtonType.YES, ButtonType.NO );
-				Optional<ButtonType> result = alert.showAndWait();
+							for ( int i = 0; i < appFunctions.serverConfigList.size(); ++i ) {
+								if ( ( "true" ).equals( appFunctions.serverConfigList.get( i ).getRunning() ) ) {
+									count++;
+								}
+							}
+							if ( count > 0 ) {
+								Alert alert = new Alert( AlertType.CONFIRMATION, "Stop all apps (" + count + ") running ?", ButtonType.YES, ButtonType.NO );
+								Optional<ButtonType> result = alert.showAndWait();
 
-				if ( result.get() == ButtonType.YES ) {
-					appFunctions.stopServers();
-					Platform.exit();
-				} else {
-					t.consume();
-				}
-				} else {
-				Platform.exit();
-				}
-			}
+								if ( result.get() == ButtonType.YES ) {
+									appFunctions.stopServers();
+									Platform.exit();
+								} else {
+									t.consume();
+								}
+							} else {
+								Platform.exit();
+							}
+						}
+					}
+				} );
 			}
 		} );
-		}
-	} );
 	}
 
 	// create Singleton instance
@@ -142,10 +142,10 @@ public class Start extends Application {
 
 
 	public static WebEngine getWebEngineInstance() {
-	if ( webEngine == null ) {
-		webEngine = webView.getEngine();
-	}
-	return webEngine;
+		if ( webEngine == null ) {
+			webEngine = webView.getEngine();
+		}
+		return webEngine;
 	}
 
 
